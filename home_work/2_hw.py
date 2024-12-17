025804bccca842e49e9ae1f5b71caad1d1ffa6be
Aleksey_Grebeshkov <grebeshkov1234@gmail.com>
2024-12-17 21:39:36 +0300

    First Commit

--- home_work/2_hw.py
+++ home_work/2_hw.py
@@ -3,7 +3,7 @@ def task_1(a:int = 1, b:float = 1.5, c:str = '2', d=None, e:bool = True) ->any:
     if d is None:
         d = [3, 4, 5]
     return type(a), type(b), type(c), type(d), type(e)
-print (task_1())
+print(task_1())
 
 
 def task_2(a=None) ->list:
@@ -15,4 +15,4 @@ print(task_2())
 
 def task_3(a) ->int:
     return a**2
-print (task_3(2))
+print(task_3(2))
