5c66093a18c39fe84d359d09383ff848d42fdca7
Aleksey_Grebeshkov <grebeshkov1234@gmail.com>
2025-01-08 20:59:49 +0300

    Commit

---
+++ tests/test_2_open_site.py
@@ -0,0 +1,15 @@
+#import time
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/")
+
+# поиск элемента
+icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
+if icon is None:
+    print('Элемент не найден')
+else:
+    print('Элемент найден')
