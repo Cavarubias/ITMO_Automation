e3bd7f83538aa4c9668bded0f5369a2b198637df
Aleksey_Grebeshkov <grebeshkov1234@gmail.com>
2025-01-11 15:53:51 +0300

    Commit

--- home_work/4_hw(car).py
+++ home_work/4_hw(car).py
@@ -1,32 +1,24 @@
 class Car:
 
-
-
     def __init__(self, color, type, year):
         self.color = color
         self.type = type
         self.year = year
 
-
     def start(self):
         print('Автомобиль заведен')
-
 
     def stop(self):
         print('Автомобиль заглушен')
 
-
     def year_of_production(self):
         print(f'Год выпуска - {self.year}')
-
 
     def type_of_car(self):
         print(f'Тип - {self.type}')
 
-
     def color_of_car(self):
         print(f'Цвет - {self.color}')
-
 
 
 attributes = Car('Черный', 'Кроссовер', '2015')
--- home_work/4_hw.py
+++ home_work/4_hw.py
@@ -1,22 +1,15 @@
 #1 Rectangle
-
 class Rectangle:
-
-
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-
     def find_area(self):
         print(self.height * self.width)
 
-
     def find_perimeter(self):
         print(2 * (self.height + self.width))
-
-
 
 
 object1 = Rectangle(2, 3)
@@ -30,34 +23,24 @@ object3.find_area()
 object3.find_perimeter()
 
 
-
-
 #2 Math
-
 class Math:
-
-
 
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
-
     def addition(self):
         print(self.a + self.b)
-
 
     def multiplication(self):
         print(self.a * self.b)
 
-
     def division(self):
         print(self.a / self.b)
 
-
     def subtraction(self):
         print(self.a - self.b)
-
 
 
 numbers = Math(50, 50)
@@ -67,10 +50,7 @@ numbers.division()
 numbers.subtraction()
 
 
-
-
 #3 SideBar (https://demoqa.com/text-box)
-
 class Sidebar:
 
     type: str = "Кнопка"
@@ -79,37 +59,34 @@ class Sidebar:
         self.text = text
         self.loc = loc
 
-
     def click(self):
         print(f'Клик по кнопке {self.text}')
 
 
-
-Text_Box = Sidebar('Text Box')
-print(Text_Box.text)
-Text_Box.click()
-Check_Box = Sidebar('Check Box')
-print(Check_Box.text)
-Check_Box.click()
-Radio_Button = Sidebar('Radio Button')
-print(Radio_Button.text)
-Radio_Button.click()
-Web_Tables = Sidebar('Web Tables')
-print(Web_Tables.text)
-Web_Tables.click()
-Buttons = Sidebar('Buttons')
-print(Buttons.text)
-Buttons.click()
-Links = Sidebar('Links')
-print(Links.text)
-Links.click()
-Broken_Links_images = Sidebar('Broken Links images')
-print(Broken_Links_images.text)
-Broken_Links_images.click()
-Upload_and_Download = Sidebar('Upload and Download')
-print(Upload_and_Download.text)
-Upload_and_Download.click()
-Dynamic_Properties = Sidebar('Dynamic Properties')
-print(Dynamic_Properties.text)
-Dynamic_Properties.click()
-
+text_box = Sidebar('Text Box')
+print(text_box.text)
+text_box.click()
+check_box = Sidebar('Check Box')
+print(check_box.text)
+check_box.click()
+radio_button = Sidebar('Radio Button')
+print(radio_button.text)
+radio_button.click()
+web_tables = Sidebar('Web Tables')
+print(web_tables.text)
+web_tables.click()
+buttons = Sidebar('Buttons')
+print(buttons.text)
+buttons.click()
+links = Sidebar('Links')
+print(links.text)
+links.click()
+broken_links_images = Sidebar('Broken Links images')
+print(broken_links_images.text)
+broken_links_images.click()
+upload_and_download = Sidebar('Upload and Download')
+print(upload_and_download.text)
+upload_and_download.click()
+dynamic_properties = Sidebar('Dynamic Properties')
+print(dynamic_properties.text)
+dynamic_properties.click()
